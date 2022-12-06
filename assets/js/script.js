7798d3ed348c7e74b59609738fa7e5a9b58b3556
Natalia Bravo <nataliabravof@gmail.com>
2022-12-06 12:14:52 -0300

    correción commit anterior

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-
-//Scroll navbar y cambio de color a cierto punto//
-$(document).scroll(function(){
-    const y = $("html").scrollTop();
-
-    y > 300 ? $("nav").assClass("nav-black") : $("nav").removeClass("nav-black")
-});
